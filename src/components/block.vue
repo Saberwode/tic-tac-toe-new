3c1b27a0c3ab0a1b1d6c56aa05c3c94901a0e20c
Saberwode <1435554189@qq.com>
2021-05-25 21:14:20 +0800

    5.25

---
+++ src/components/block.vue
@@ -0,0 +1,93 @@
+<template>
+  <!-- <div class="box" @click="changePlayer(i, j)">
+    {{ childstats[i - 1][j - 1].id }}
+  </div>
+  <div v-for="i in 3" :key="i">
+    <div class="body">
+      <div v-for="j in 3" :key="j">
+        <block :i="i" :j="j"></block>
+      </div>
+    </div>
+  </div> -->
+  <div>
+    <div v-for="i in 3" :key="i">
+      <div class="body">
+        <div v-for="j in 3" :key="j">
+          <div class="box" @click="changePlayer(i, j)">
+            {{ childstats[i - 1][j - 1].player }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "block",
+  data() {
+    return {
+      childstats: [
+        [
+          { id: "1", click_state: true, player: "" },
+          { id: "2", click_state: true, player: "" },
+          { id: "3", click_state: true, player: "" },
+        ],
+        [
+          { id: "4", click_state: true, player: "" },
+          { id: "5", click_state: true, player: "" },
+          { id: "6", click_state: true, player: "" },
+        ],
+        [
+          { id: "7", click_state: true, player: "" },
+          { id: "8", click_state: true, player: "" },
+          { id: "9", click_state: true, player: "" },
+        ],
+      ],
+      nextPlayer: "O",
+    };
+  },
+  props: [],
+  methods: {
+    changePlayer(i, j) {
+      // console.log("nextPlayer");
+      // console.log(this.nextPlayer);
+      if (!this.childstats[i - 1][j - 1].click_state) return;
+      this.childstats[i - 1][j - 1].player = this.nextPlayer;
+      this.childstats[i - 1][j - 1].click_state = false;
+      if (this.nextPlayer == "O") {
+        this.nextPlayer = "X";
+      } else if ((this.nextPlayer = "X")) {
+        this.nextPlayer = "O";
+      }
+      console.log("当前点击的player");
+      console.log(this.childstats[i - 1][j - 1].player);
+      // console.log("转换后的player");
+      // console.log(this.nextPlayer);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.box {
+  background: #fff;
+  margin: 0 auto;
+  width: 100px;
+  height: 100px;
+  text-align: center;
+  line-height: 100px;
+  float: left;
+  border-left: 1px solid black;
+  border-bottom: 1px solid black;
+}
+
+.body {
+  width: 303px;
+  height: 100px;
+  margin: 0 auto;
+  display: block;
+  border-right: 1px solid black;
+  border-top: 1px solid black;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <block></block>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import block from "@/components/block.vue";
 
 export default {
-  name: 'Home',
+  name: "app",
+  data() {
+    return {};
+  },
   components: {
-    HelloWorld
-  }
-}
+    block,
+  },
+};
 </script>
+
+<style scoped>
+.box {
+  background: #fff;
+  margin: 0 auto;
+  width: 100px;
+  height: 100px;
+  text-align: center;
+  line-height: 100px;
+  float: left;
+  border-left: 1px solid black;
+  border-bottom: 1px solid black;
+}
+
+.body {
+  width: 303px;
+  height: 100px;
+  margin: 0 auto;
+  display: block;
+  border-right: 1px solid black;
+  border-top: 1px solid black;
+}
+</style>
